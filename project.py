49975dc585dc304517fa115034bdb5489340d418
Jose Caballero Bejar <jcaballero.hep@gmail.com>
2026-04-14 10:59:34 +0100

    fixed

--- project.py
+++ project.py
@@ -323,9 +323,10 @@ class Project:
         # --group 43eba3ef7244a53f90f5d114fbf3f3ce0578a08f430aca380ff597d6a40381b3 
         # --group-domain jasmin 
         # user
-        print("hi")
-        #cmd = f'openstack --os-cloud admin role add --project {self.id} --group {group.id} --group-domain {group.domain.id} user'
-        #results = run(cmd)
+
+        print(group.domain.id)
+        cmd = f'openstack --os-cloud {self._cloud.cloud} role add --project {self.id} --group {group.id} --group-domain {group.domain.id} user'
+        results = run(cmd)
 
     def _add_flavor(self, flavor):
         # command:
